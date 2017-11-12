c2f8bb88600132152e00338b402185f7b67829ee
ywang <yong.wang@ucarinc.com>
2017-11-12 21:51:22 +0800

    第一章,hello world

--- chapter01/main.cpp
+++ chapter01/main.cpp
@@ -10,7 +10,6 @@ int main() {
     std::cout << "The sum of " << v1 << " and "
                               << v2 << " is " << v1 + v2 << std::endl;
 
-
     //成功返回值 0 ,非0 代表含义需要参考系统编码
     return 0;
 }
